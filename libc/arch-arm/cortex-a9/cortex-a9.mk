db17b0b85c4cc896ecd0b245b09d09dcec84be3b
RyanG <decatf@gmail.com>
2015-03-11 18:50:25 -0400

    Add stpcpy to cortex-a9

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -1,13 +1,11 @@
-
-
 $(call libc-add-cpu-variant-src,STRCAT,arch-arm/cortex-a9/bionic/strcat.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/cortex-a9/bionic/strcmp.S)
+$(call libc-add-cpu-variant-src,STPCPY,arch-arm/cortex-a9/bionic/stpcpy.S)
 
 ifeq ($(strip $(ARCH_ARM_HAVE_NEON)),true)
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/cortex-a9/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/cortex-a9/bionic/memset.S)
 
-$(call libc-add-cpu-variant-src,STPCPY,arch-arm/cortex-a9/bionic/stpcpy.S)
 $(call libc-add-cpu-variant-src,STRCPY,arch-arm/cortex-a9/bionic/strcpy.S)
 $(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a9/bionic/strlen.S)
 
