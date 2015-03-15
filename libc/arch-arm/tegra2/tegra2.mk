51d01001a88733b8abe0dda5c5abcc589cdabd50
RyanG <decatf@gmail.com>
2015-03-15 13:11:10 -0400

    libc: Update tegra2
     - Move some parts from generic to Cortex-A9
     - Update strlen.S

--- libc/arch-arm/tegra2/tegra2.mk
+++ libc/arch-arm/tegra2/tegra2.mk
@@ -1,13 +1,17 @@
+# From arch-arm generic
 libc_bionic_src_files_arm += \
 	arch-arm/generic/bionic/memcpy.S \
 	arch-arm/generic/bionic/memset.S \
-	upstream-openbsd/lib/libc/string/stpcpy.c \
-	upstream-openbsd/lib/libc/string/strcat.c \
-	arch-arm/cortex-a15/bionic/strcmp.S \
 	arch-arm/generic/bionic/strcpy.S \
 	bionic/__strcat_chk.cpp \
 	bionic/__strcpy_chk.cpp \
 	bionic/memmove.c
+
+# non-NEON compatible cortex-a9
+libc_bionic_src_files_arm += \
+	arch-arm/cortex-a9/bionic/stpcpy.S \
+	arch-arm/cortex-a9/bionic/strcat.S \
+	arch-arm/cortex-a9/bionic/strcmp.S \
 
 # armv7-a (non neon) ones from cm10.1
 libc_bionic_src_files_arm += \
