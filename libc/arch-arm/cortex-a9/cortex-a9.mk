7eab128803c9fda2f50727b1ed633d5135004868
RyanG <decatf@gmail.com>
2015-04-06 11:51:56 -0400

    libc: Restore cortex-a9 to aosp

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -1,19 +1,34 @@
-$(call libc-add-cpu-variant-src,STRCAT,arch-arm/cortex-a9/bionic/strcat.S)
-$(call libc-add-cpu-variant-src,STRCMP,arch-arm/cortex-a9/bionic/strcmp.S)
-$(call libc-add-cpu-variant-src,STPCPY,arch-arm/cortex-a9/bionic/stpcpy.S)
-
 ifeq ($(strip $(ARCH_ARM_HAVE_NEON)),true)
-$(call libc-add-cpu-variant-src,MEMCPY,arch-arm/cortex-a9/bionic/memcpy.S)
-$(call libc-add-cpu-variant-src,MEMSET,arch-arm/cortex-a9/bionic/memset.S)
-
-$(call libc-add-cpu-variant-src,STRCPY,arch-arm/cortex-a9/bionic/strcpy.S)
-$(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a9/bionic/strlen.S)
-
-$(call libc-add-cpu-variant-src,__STRCAT_CHK,arch-arm/cortex-a9/bionic/__strcat_chk.S)
-$(call libc-add-cpu-variant-src,__STRCPY_CHK,arch-arm/cortex-a9/bionic/__strcpy_chk.S)
+libc_bionic_src_files_arm += \
+    bionic/memchr.c \
+    arch-arm/cortex-a9/bionic/memcpy.S \
+    arch-arm/cortex-a9/bionic/memset.S \
+    arch-arm/cortex-a9/bionic/stpcpy.S \
+    arch-arm/cortex-a9/bionic/strcat.S \
+    arch-arm/cortex-a9/bionic/__strcat_chk.S \
+    arch-arm/cortex-a9/bionic/strcmp.S \
+    arch-arm/cortex-a9/bionic/strcpy.S \
+    arch-arm/cortex-a9/bionic/__strcpy_chk.S \
+    arch-arm/cortex-a9/bionic/strlen.S \
+    bionic/memmove.c \
 else
-$(call libc-add-cpu-variant-src,STRCPY,arch-arm/cortex-a9/bionic-linaro/strcpy.c)
-$(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a9/bionic-linaro/strlen.S)
-endif
+# non-neon cortex-a9 (e.g. Tegra 2)
 
-include bionic/libc/arch-arm/generic/generic.mk
+# From arch-arm generic
+libc_bionic_src_files_arm += \
+    bionic/memmove.c
+    arch-arm/generic/bionic/memcpy.S \
+    arch-arm/generic/bionic/memset.S \
+    arch-arm/generic/bionic/strcpy.S \
+    bionic/__strcat_chk.cpp \
+    bionic/__strcpy_chk.cpp
+
+# non-NEON compatible cortex-a9
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a9/bionic/stpcpy.S \
+    arch-arm/cortex-a9/bionic/strcat.S \
+    arch-arm/cortex-a9/bionic/strcmp.S \
+    arch-arm/cortex-a9/bionic-linaro/strlen.S \
+    arch-arm/cortex-a9/bionic-linaro/memchr.S
+
+endif
