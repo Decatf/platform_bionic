4794b0616b9f556a8e509b69867cbfd3c2db3cd0
RyanG <decatf@gmail.com>
2015-04-12 13:48:34 -0400

    libc: tegra2: Cortex-A9 optimized memcpy

--- libc/arch-arm/tegra2/tegra2.mk
+++ libc/arch-arm/tegra2/tegra2.mk
@@ -1,6 +1,5 @@
 # From arch-arm generic
 libc_bionic_src_files_arm += \
-	arch-arm/generic/bionic/memcpy.S \
 	arch-arm/generic/bionic/memset.S \
 	arch-arm/generic/bionic/strcpy.S \
 	bionic/__strcat_chk.cpp \
@@ -17,3 +16,7 @@ libc_bionic_src_files_arm += \
 libc_bionic_src_files_arm += \
 	arch-arm/tegra2/bionic/strlen.S \
 	arch-arm/tegra2/bionic/memchr.S
+
+libc_common_src_files_arm += \
+	bionic/__memcpy_chk.cpp \
+	arch-arm/bionic/memcpy.a9.S \
