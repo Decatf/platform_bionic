f354b2abb46479a3b3845af8561c7e2b492b3658
RyanG <decatf@gmail.com>
2015-04-25 11:55:28 -0400

    Revert "libc: tegra2: Cortex-A9 optimized memcpy"
    This reverts commit 4794b0616b9f556a8e509b69867cbfd3c2db3cd0.

--- libc/arch-arm/tegra2/tegra2.mk
+++ libc/arch-arm/tegra2/tegra2.mk
@@ -1,5 +1,6 @@
 # From arch-arm generic
 libc_bionic_src_files_arm += \
+	arch-arm/generic/bionic/memcpy.S \
 	arch-arm/generic/bionic/memset.S \
 	arch-arm/generic/bionic/strcpy.S \
 	bionic/__strcat_chk.cpp \
@@ -16,7 +17,3 @@ libc_bionic_src_files_arm += \
 libc_bionic_src_files_arm += \
 	arch-arm/tegra2/bionic/strlen.S \
 	arch-arm/tegra2/bionic/memchr.S
-
-libc_common_src_files_arm += \
-	bionic/__memcpy_chk.cpp \
-	arch-arm/bionic/memcpy.a9.S \
