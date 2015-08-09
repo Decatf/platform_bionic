b95b9b725b4c1fc1bd3b23bffdcb0abca4429e9d
RyanG <decatf@gmail.com>
2015-08-09 12:15:03 -0400

    libc: tegra2: Use a15 strlen and string copy
    Depends on:
    "Replace Cortex A15 strlen implementation with cortex-strings"
    "Import memchr implementation from cortex-strings for Cortex A15"
    
    
    A15 strlen performance is slightly lower for smaller strings but
    significantly faster for larger strings. It is also more consistent.
    
    Before:
                                     iterations      ns/op
    BM_string_strlen/8                100000000         27   286.67 MiB/s
    BM_string_strlen/64                50000000         73   866.05 MiB/s
    BM_string_strlen/512                5000000        389  1314.21 MiB/s
    BM_string_strlen/1K                 5000000        744  1375.72 MiB/s
    BM_string_strlen/8K                  500000       5715  1433.33 MiB/s
    BM_string_strlen/16K                 200000      11406  1436.35 MiB/s
    BM_string_strlen/32K                  50000      30413  1077.42 MiB/s
    BM_string_strlen/64K                  20000      96026   682.48 MiB/s
    
    After:
                                     iterations      ns/op
    BM_string_strlen/8                 50000000         30   262.56 MiB/s
    BM_string_strlen/64                50000000         72   880.51 MiB/s
    BM_string_strlen/512                5000000        426  1201.83 MiB/s
    BM_string_strlen/1K                 2000000        863  1185.59 MiB/s
    BM_string_strlen/8K                  500000       6577  1245.48 MiB/s
    BM_string_strlen/16K                 200000      13041  1256.26 MiB/s
    BM_string_strlen/32K                 100000      27320  1199.39 MiB/s
    BM_string_strlen/64K                  50000      52795  1241.32 MiB/s

--- libc/arch-arm/tegra2/tegra2.mk
+++ libc/arch-arm/tegra2/tegra2.mk
@@ -6,14 +6,12 @@ libc_bionic_src_files_arm += \
 	bionic/__strcpy_chk.cpp \
 	bionic/memmove.c
 
-# non-NEON compatible cortex-a9
 libc_bionic_src_files_arm += \
-	arch-arm/cortex-a9/bionic/stpcpy.S \
 	arch-arm/cortex-a9/bionic/strcat.S \
 	arch-arm/cortex-a9/bionic/strcmp.S \
-	arch-arm/cortex-a9/bionic/strcpy.S \
 
-# armv7-a (non neon) ones from cm10.1
 libc_bionic_src_files_arm += \
-	arch-arm/tegra2/bionic/strlen.S \
-	arch-arm/tegra2/bionic/memchr.S
+	arch-arm/cortex-a15/bionic/strlen.S \
+	arch-arm/cortex-a15/bionic/stpcpy.S \
+	arch-arm/cortex-a15/bionic/strcpy.S \
+	arch-arm/cortex-a15/bionic/memchr.S
