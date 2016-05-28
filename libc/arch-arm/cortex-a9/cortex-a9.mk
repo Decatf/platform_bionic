eda9a8ae966d7dac4e247a12086a7383ecfe10f4
RyanG <decatf@gmail.com>
2016-05-28 13:08:39 -0400

    libc: Use cortex-a9 strlen

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -2,7 +2,8 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a9/bionic/strcat.S \
     arch-arm/cortex-a9/bionic/strcmp.S \
     arch-arm/cortex-a9/bionic/stpcpy.S \
-    arch-arm/cortex-a9/bionic/strcpy.S
+    arch-arm/cortex-a9/bionic/strcpy.S \
+    arch-arm/cortex-a9/bionic/strlen.S \
 
 libc_bionic_src_files_arm += \
     arch-arm/generic/bionic/memchr.S \
@@ -15,7 +16,6 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a9/bionic/memset.S \
     arch-arm/cortex-a9/bionic/__strcat_chk.S \
     arch-arm/cortex-a9/bionic/__strcpy_chk.S \
-    arch-arm/cortex-a9/bionic/strlen.S \
 
 libc_bionic_src_files_arm += \
     arch-arm/denver/bionic/memmove.S \
@@ -27,7 +27,6 @@ libc_bionic_src_files_arm += \
     arch-arm/generic/bionic/memset.S \
     bionic/__strcat_chk.cpp \
     bionic/__strcpy_chk.cpp \
-    arch-arm/cortex-a15/bionic/strlen.S \
 
 libc_bionic_src_files_arm += \
     upstream-openbsd/lib/libc/string/memmove.c \
